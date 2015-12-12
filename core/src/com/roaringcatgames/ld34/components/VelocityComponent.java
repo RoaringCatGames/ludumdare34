ab5b78aba0f366e12a3ba1ebfcaf1a98f6ee2a13
Barryrowe <barryrowe@gmail.com>
2015-12-12 01:12:22 -0500

    Add packer directories
    Adding packer directories.

---
+++ core/src/com/roaringcatgames/ld34/components/VelocityComponent.java
@@ -0,0 +1,11 @@
+package com.roaringcatgames.ld34.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by barry on 12/12/15 @ 1:07 AM.
+ */
+public class VelocityComponent implements Component {
+    public float x = 0f;
+    public float y = 0f;
+}
